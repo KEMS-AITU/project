4a8f2466f9b5005d7861b5d02ca41165c05d7e86
Impala4c <mansuradan9@gmail.com>
2026-01-17 14:59:11 +0500

    url path added

--- complaint_system/urls.py
+++ complaint_system/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
+    path('',include('feedback.urls')),
     path('admin/', admin.site.urls),
     path('api/', include('feedback.urls'))
 ]
